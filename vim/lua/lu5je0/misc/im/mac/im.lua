9d22efb9c066464da209ee117a41543c26a9b731
zuoming <zuoming@souche.com>
2022-12-02 14:49:01 +0800

    error fix

--- vim/lua/lu5je0/misc/im/mac/im.lua
+++ vim/lua/lu5je0/misc/im/mac/im.lua
@@ -17,7 +17,7 @@ local im_switcher = (function()
   return {
     switch_to_im = function(im_code)
       ---@diagnostic disable-next-line: undefined-field
-      switcher.switchInputSource(string.char(im_code))
+      switcher.switchInputSource(im_code)
     end
   }
 end)()
