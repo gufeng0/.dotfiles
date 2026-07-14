572f400afa08e43578ddbbaa9dda3b897636ebd8
shuaixiaomi <840903654@qq.com>
2026-07-14 20:17:25 +0800

    feat: support custom proxy IP

--- zsh/proxy.sh
+++ zsh/proxy.sh
@@ -2,10 +2,13 @@ export USER_HTTP_PROXY=${USER_HTTP_PROXY:-'http://127.0.0.1:1080'}
 export USER_SOCKS_PROXY=${USER_SOCKS_PROXY:-'socks5://127.0.0.1:1080'}
 
 proxy() {
-  export http_proxy=$USER_HTTP_PROXY
-  export HTTP_PROXY=$USER_HTTP_PROXY
-  export https_proxy=$USER_HTTP_PROXY
-  export HTTPS_PROXY=$USER_HTTP_PROXY
+  local proxy_ip=${1:-127.0.0.1}
+  local user_http_proxy=${USER_HTTP_PROXY/127.0.0.1/$proxy_ip}
+
+  export http_proxy=$user_http_proxy
+  export HTTP_PROXY=$user_http_proxy
+  export https_proxy=$user_http_proxy
+  export HTTPS_PROXY=$user_http_proxy
 }
 
 unproxy() {
